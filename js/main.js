ca94d9632c5bf9f90cdc6a38ca30599bc027ded6
valsementsov <sementsov@digitalaware.dev>
2022-10-19 19:12:18 +0600

    first_commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Burger handler
+(function ()    {
+    const burgerItem = document.querySelector('.header_burger');
+    burgerItem.addEventListener('click', () => {
+        console.log(1);
+    })
+}());
